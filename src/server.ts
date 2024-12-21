695015b23f42857ce81ad6a15a1ca44763f8c5d4
Luis Starlino <luis.guilherme009@gmail.com>
2024-12-21 17:08:19 -0300

    creating a first route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+/*****************************************************************************************
+* @Author: Luis Starlino
+* @Date: 2024-12-21 17:00
+* @Description: Minimal API
+*****************************************************************************************/
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+server.get("/api/teams", async (req, res) => {
+    res.type("application/json").code(200)
+
+    return [
+        { id: 1, name: "Ferrari" }
+    ]
+})
+
+server.listen({ port: 3333 }, () => {
+    console.log("SERVER INIT")
+})

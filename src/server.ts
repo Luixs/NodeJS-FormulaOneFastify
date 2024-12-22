e69afab6f50373ba6117b6051b3aa72dd66ad16e
Luis Starlino <luis.guilherme009@gmail.com>
2024-12-22 14:23:09 -0300

    feat(drivers): add driver route by filter

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,49 @@
 *****************************************************************************************/
 import fastify from 'fastify';
 
+// ===== CONSTS
 const server = fastify({ logger: true });
+const dummyTeams = [
+    { id: 1, name: "McLaren", base: "United Kingdom" },
+    { id: 2, name: "Mercedes", base: "United Kingdom" },
+    { id: 3, name: "Red Bull Racing", base: "United Kingdom" }
+]
+const dummyDrivers = [
+    { id: 1, name: "Max Verstappen", team: "Red bull Racing" },
+    { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+    { id: 3, name: "Lando Norris", team: "McLaren" }
+]
 
 server.get("/api/teams", async (req, res) => {
     res.type("application/json").code(200)
 
-    return [
-        { id: 1, name: "Ferrari" }
-    ]
+    return { dummyTeams };
 })
+
+server.get("/api/drivers", async (req, res) => {
+    res.type("application/json").code(200)
+
+    return { dummyDrivers }
+})
+
+interface DriverParams {
+    id: string
+}
+
+server.get<{ Params: DriverParams }>("/api/drivers/:id", async (req, res) => {
+
+    // ===== GET PARAMS & SEARCH DATA
+    const id = parseInt(req.params.id);
+    const driver = dummyDrivers.find(d => d.id === id);
+
+    if (!driver) {
+        res.type("application/json").code(204);
+        return { message: "Driver not found!" };
+    }
+    res.type("application/json").code(200)
+    return { driver };
+
+});
 
 server.listen({ port: 3333 }, () => {
     console.log("SERVER INIT")
